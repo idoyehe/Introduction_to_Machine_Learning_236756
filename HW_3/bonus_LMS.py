1b712bb1bd9849cdf7181f140f78ae1a53a2562f
IdoYe <idoye@campus.technion.acl>
2019-05-22 20:19:29 +0300

    iris and digits comparing DONE

--- HW_3/bonus_LMS.py
+++ HW_3/bonus_LMS.py
@@ -6,65 +6,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def one_vs_all(clf, x_train, y_train, class_index, x_val=None, y_val=None):
-    y_train_modified = y_train.astype('int').copy()
-    y_train_modified = np.vectorize(lambda t: 1 if t == class_index else -1)(y_train_modified)
-    if x_val is None or y_val is None:
-        return clf.fit(x_train, y_train_modified)
-    y_val_modified = y_val.astype('int').copy()
-    y_val_modified = np.vectorize(lambda t: 1 if t == class_index else -1)(y_val_modified)
-    return clf.fit(x_train, y_train_modified, x_val, y_val_modified)
+class AdalineSGD(object):
 
-
-class LMS:
-    """ LMS class that implements Widrow-Hoff algorithm """
-
-    def __init__(self, eta, max_epochs: int = None):
+    def __init__(self, eta=0.01, epochs=50):
         self.eta = eta
-        self.max_epochs = max_epochs
+        self.epochs = epochs
         self.weights = None
-        self.train_stats = None
-        self.valid_stats = None
-        self.weights_stats = None
-        self.best_epoch = None
+        self.cost = None
 
-    def fit(self, x_train, y_train, x_val, y_val):
-        # initialize weights and bias
-        self._init_weights_vector((x_train.shape[1], 1))
-        self.train_stats = []
-        self.valid_stats = []
-        self.weights_stats = []
-        self.best_epoch = None
-        for epoch in range(self.max_epochs):
+    def train(self, x_train, y_train, reinitialize_weights=True):
+
+        if reinitialize_weights:
+            self.weights = np.zeros(1 + x_train.shape[1])
+        self.cost = []
+
+        for i in range(self.epochs):
             for xi, target in zip(x_train, y_train):
-                y_pred_i = self._calc_y_pred(xi.reshape((x_train.shape[1], 1)))
-                self.weights += self.eta * (target - y_pred_i) * xi.reshape((x_train.shape[1], 1))
+                output = self.net_input(xi)
+                error = (target - output)
+                self.weights[1:] += self.eta * xi.dot(error)
+                self.weights[0] += self.eta * error
 
-            self.train_stats.append(1 - accuracy_score(y_true=y_train, y_pred=self.predict(x_train)))
-            self.valid_stats.append(1 - accuracy_score(y_true=y_val, y_pred=self.predict(x_val)))
-            self.weights_stats.append(self.weights.copy())
-
-        self.best_epoch = self.valid_stats.index(min(self.valid_stats))
-        self.weights = self.weights_stats[self.best_epoch]
+            cost = ((y_train - self.activation(x_train)) ** 2).sum() / 2.0
+            self.cost.append(cost)
         return self
 
-    def _calc_y_pred(self, x_i):
-        scalar = self.weights.T @ x_i
-        return scalar[0][0]
+    def net_input(self, X):
+        return np.dot(X, self.weights[1:]) + self.weights[0]
 
-    def _init_weights_vector(self, weights_shape):
-        self.weights = np.random.uniform(-1, 1, weights_shape)
+    def activation(self, X):
+        return self.net_input(X)
 
-    def calc_mse(self, y, y_pred):
-        mse_error = y - y_pred
-        mse_error = (mse_error ** 2).sum() / 2.0
-        return mse_error
-
-    def predict(self, x):
-        return np.sign(x @ self.weights)
+    def predict(self, X):
+        return np.where(self.activation(X) >= 0.0, 1, -1)
 
 
-def load_iris_dataset(test_size=0.15):
+def one_vs_all(y, class_index):
+    return np.vectorize(lambda t: 1 if t == class_index else -1)(y.astype('int').copy())
+
+
+def load_iris_dataset(test_size=0.30):
     iris = load_iris()
     sss = StratifiedShuffleSplit(random_state=0, n_splits=1, test_size=test_size)
     x, y = iris.data, iris.target
@@ -90,70 +71,96 @@ def load_digits_dataset(test_size=0.15):
     return x_train, x_test, y_train, y_test
 
 
-def clf_evaluation(clf, clf_title, x_test, y_test, class_index):
-    y_pred = np.sign(clf.predict(x_test))
-    y_test_modified = y_test.astype('int').copy()
-    y_test_modified = np.vectorize(lambda t: 1 if t == class_index else -1)(y_test_modified)
-    print(f"{clf_title} accuracy score: {100 * accuracy_score(y_pred=y_pred, y_true=y_test_modified)}%")
+def __lms_evaluation(current_class, x_train, x_test, y_train, y_test, lr, max_epochs):
+    print(f"current class is {current_class}")
+    train_error = []
+    test_error = []
+    for current_max in range(max_epochs):
+        adaline = AdalineSGD(eta=lr, epochs=current_max)
+        adaline.train(x_train, one_vs_all(y_train, current_class))
+        y_train_pred = adaline.predict(x_train)
+        y_test_pred = adaline.predict(x_test)
+        train_error.append(1 - accuracy_score(y_true=one_vs_all(y_train, current_class), y_pred=y_train_pred))
+        test_error.append(1 - accuracy_score(y_true=one_vs_all(y_test, current_class), y_pred=y_test_pred))
+
+    plt.plot(train_error, 'b', label="Train")
+    plt.plot(test_error, 'r', label="Test")
+    plt.xlabel("# Epochs")
+    plt.ylabel("Error rate")
+    plt.legend()
+    min_error = min(test_error)
+    epoch = test_error.index(min_error)
+    plt.show()
+    print(f"convergence epoch is: {epoch}")
+    print(f"Train: {100 - 100 * train_error[epoch]:.2f}%")
+    print(f"Test: {100 - 100 * test_error[epoch]:.2f}%")
 
 
-def lms_evaluation(dataset_name, classes, lms_clf,
-                   x_train, x_test, y_train, y_test):
+def lms_iris_and_digits():
+    # print("Iris Dataset LMS")
+    # iris_x_train, iris_x_test, iris_y_train, iris_y_test = load_iris_dataset(test_size=0.30)
+    # for current_class in range(3):
+    #     __lms_evaluation(current_class, iris_x_train, iris_x_test, iris_y_train, iris_y_test, 0.001, 300)
 
-    print(f"{dataset_name} Dataset")
-    # perceptron_clf = Perceptron(random_state=0, alpha=lr)  # , verbose=True)
-
-    def comparing(_curr_class):
-        print(f"current class {_curr_class}")
-        lms_clf_fitted = one_vs_all(lms_clf, np.append(x_train, np.ones((x_train.shape[0], 1)), axis=1),
-                                    y_train, _curr_class, np.append(x_test, np.ones((x_test.shape[0], 1)), axis=1),
-                                    y_test)
-        print(f"LMS cover in {lms_clf_fitted.best_epoch} iteration")
-        plt.plot(lms_clf_fitted.valid_stats, 'b', label="validation")
-        plt.plot(lms_clf_fitted.train_stats, 'r', label="train")
-        plt.xlabel("# Epochs")
-        plt.ylabel("Validation Error rate")
-        plt.legend()
-        plt.show()
-
-        clf_evaluation(lms_clf_fitted, "LMS", np.append(x_test, np.ones((x_test.shape[0], 1)), axis=1), y_test, _curr_class)
-
-        # perceptron_clf_fitted = one_vs_all(perceptron_clf, x_train, y_train, _curr_class)
-        # clf_evaluation(perceptron_clf_fitted, "Perceptron", x_test, y_test, _curr_class)
-
-    if classes > 2:
-        for curr_class in range(classes):
-            comparing(curr_class)
-    else:
-        comparing(0)
+    print("Digits Dataset LMS")
+    digits_x_train, digits_x_test, digits_y_train, digits_y_test = load_digits_dataset(test_size=0.30)
+    for current_class in range(10):
+        __lms_evaluation(current_class, digits_x_train, digits_x_test, digits_y_train, digits_y_test, 0.001, 100)
 
 
-def loading_dataset_for_lms(test_size=0.15):
-    x, y = make_classification(
-        n_samples=1000,
-        n_features=4,
-        n_informative=1,
-        n_redundant=2,
-        n_classes=2,
-        n_clusters_per_class=1,
-        weights=None,
-        flip_y=0.1,
-        class_sep=0.000001,
-        hypercube=True,
-        shift=0.0,
-        scale=1.0,
-        shuffle=True,
-        random_state=0)
-    sss = StratifiedShuffleSplit(n_splits=1, test_size=test_size)
-    train_index_first, test_index = next(sss.split(x, y))
-    x_train, x_test, y_train, y_test = x[train_index_first], x[test_index], y[train_index_first], y[test_index]
-    return x_train, x_test, y_train, y_test
+def __perceptron_evaluation(classes, x_train, x_test, y_train, y_test, lr, max_epochs):
+    for current_class in range(classes):
+        print(f"current class is {current_class}")
+
+        perceptron = Perceptron(alpha=lr, max_iter=max_epochs, verbose=True, tol=1e-3)
+        perceptron.fit(x_train, one_vs_all(y_train, current_class))
+        y_train_pred = perceptron.predict(x_train)
+        y_test_pred = perceptron.predict(x_test)
+        train_error = 1 - accuracy_score(y_true=one_vs_all(y_train, current_class), y_pred=y_train_pred)
+        test_error = 1 - accuracy_score(y_true=one_vs_all(y_test, current_class), y_pred=y_test_pred)
+
+        print(f"Train: {100 - 100 * train_error:.2f}%")
+        print(f"Test: {100 - 100 * test_error:.2f}%")
+
+
+def perceptron_iris_and_digits():
+    print("Iris Dataset Perceptron")
+    iris_x_train, iris_x_test, iris_y_train, iris_y_test = load_iris_dataset(test_size=0.30)
+    __perceptron_evaluation(3, iris_x_train, iris_x_test, iris_y_train, iris_y_test, 0.0001, 300)
+
+    print("Digits Dataset Perceptron")
+    digits_x_train, digits_x_test, digits_y_train, digits_y_test = load_digits_dataset(test_size=0.30)
+    __perceptron_evaluation(10, digits_x_train, digits_x_test, digits_y_train, digits_y_test, 0.0001, 300)
+
+
+# def loading_dataset_for_lms(test_size=0.15):
+#     x, y = make_classification(
+#         n_samples=1000,
+#         n_features=4,
+#         n_informative=1,
+#         n_redundant=2,
+#         n_classes=2,
+#         n_clusters_per_class=1,
+#         weights=None,
+#         flip_y=0.1,
+#         class_sep=0.000001,
+#         hypercube=True,
+#         shift=0.0,
+#         scale=1.0,
+#         shuffle=True,
+#         random_state=0)
+#     sss = StratifiedShuffleSplit(n_splits=1, test_size=test_size)
+#     train_index_first, test_index = next(sss.split(x, y))
+#     x_train, x_test, y_train, y_test = x[train_index_first], x[test_index], y[train_index_first], y[test_index]
+#     return x_train, x_test, y_train, y_test
 
 
 if __name__ == '__main__':
     np.random.seed(0)
-    iris_x_train, iris_x_test, iris_y_train, iris_y_test = load_iris_dataset(test_size=0.30)
-    # lms_vs_perceptron(load_iris_dataset, "Iris", 3)
-    lms_clf = LMS(eta=0.001, max_epochs=500)
-    lms_evaluation(load_digits_dataset, "Digits", 10)
-    # lms_vs_perceptron(loading_dataset_for_lms, "LMS better", 2, lms_max_iter=5000000, perceptron_max_iter=12)
+    lms_iris_and_digits()
+    # perceptron_iris_and_digits()
+
+    # # lms_vs_perceptron(load_iris_dataset, "Iris", 3)
+    # lms_clf = LMS(eta=0.001, max_epochs=500)
+    # lms_evaluation(load_digits_dataset, "Digits", 10)
+    # # lms_vs_perceptron(loading_dataset_for_lms, "LMS better", 2, lms_max_iter=5000000, perceptron_max_iter=12)
