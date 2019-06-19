9d4b0d22309a130dbc8d08a92b668c4234bae011
idoye <idoye@campus.technion.ac.il>
2019-06-19 10:57:17 +0300

    HW_5 data preparation

---
+++ HW_5/data_infrastructure.py
@@ -0,0 +1,202 @@
+from os import path
+from pandas import DataFrame, read_csv
+import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import cross_val_score
+import operator
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+
+PATH = path.dirname(path.realpath(__file__)) + "/"
+
+TRAINING_SET_PATH = PATH + "ElectionsData.csv"
+TEST_SET_PATH = PATH + "ElectionsData_Pred_Features.csv"
+
+TRAIN_PATH = PATH + "fixed_train.csv"
+VALIDATION_PATH = PATH + "fixed_val.csv"
+TEST_PATH = PATH + "fixed_test.csv"
+
+# constants
+global_train_size = 0.65
+global_test_size = 0.25
+global_validation_size = 0.1
+assert global_train_size + global_test_size + global_validation_size == 1
+global_z_threshold = 4.5
+global_correlation_threshold = 0.9
+global_transportation_threshold = 0.8
+label = 'Vote'
+global_party_in_coalition_threshold = 0.7
+
+# lists
+voters_id = "IdentityCard_Num"
+
+selected_features = ['Vote', 'Most_Important_Issue', 'Avg_government_satisfaction',
+                     'Avg_education_importance', 'Avg_environmental_importance',
+                     'Avg_Residancy_Altitude', 'Yearly_ExpensesK',
+                     'Avg_monthly_expense_on_pets_or_plants', 'Weighted_education_rank',
+                     'Number_of_valued_Kneset_members']
+
+selected_features_without_label = ['Avg_Residancy_Altitude', 'Avg_education_importance', 'Avg_environmental_importance',
+                                   'Avg_government_satisfaction', 'Avg_monthly_expense_on_pets_or_plants', 'Most_Important_Issue',
+                                   'Number_of_valued_Kneset_members', 'Weighted_education_rank', 'Yearly_ExpensesK']
+
+selected_nominal_features = ['Most_Important_Issue']
+
+selected_numerical_features = ['Avg_Residancy_Altitude', 'Avg_education_importance', 'Avg_environmental_importance',
+                               'Avg_government_satisfaction', 'Avg_monthly_expense_on_pets_or_plants', 'Number_of_valued_Kneset_members',
+                               'Weighted_education_rank', 'Yearly_ExpensesK']
+
+selected_uniform_features = ['Avg_government_satisfaction', 'Avg_education_importance',
+                             'Avg_environmental_importance',
+                             'Avg_Residancy_Altitude', 'Yearly_ExpensesK']
+
+selected_normal_features = ['Avg_monthly_expense_on_pets_or_plants',
+                            'Number_of_valued_Kneset_members']
+
+label2num = {'Blues': 0, 'Browns': 1, 'Greens': 2, 'Greys': 3, 'Khakis': 4, 'Oranges': 5, 'Pinks': 6, 'Purples': 7, 'Reds': 8,
+             'Turquoises': 9, 'Violets': 10, 'Whites': 11, 'Yellows': 12}
+
+num2label = {0: 'Blues', 1: 'Browns', 2: 'Greens', 3: 'Greys', 4: 'Khakis', 5: 'Oranges', 6: 'Pinks', 7: 'Purples', 8: 'Reds',
+             9: 'Turquoises', 10: 'Violets', 11: 'Whites', 12: 'Yellows'}
+
+
+def load_data(filepath: str) -> DataFrame:
+    df = read_csv(filepath, header=0)
+    return df
+
+
+def load_prepared_dataFrames():
+    return load_data(TRAIN_PATH), load_data(VALIDATION_PATH), load_data(TEST_PATH)
+
+
+def filter_possible_coalitions(possible_coalitions: dict):
+    """
+    :param possible_coalitions: all possible coalition
+    :return: possible coalition without duplication
+    """
+    # remove duplicates
+    filtered_possible_coalitions = dict()
+    for _coalition_name, _coalition_list in possible_coalitions.items():
+        _coalition_list.sort()
+        if _coalition_list not in filtered_possible_coalitions.values():
+            filtered_possible_coalitions[_coalition_name] = _coalition_list
+    return filtered_possible_coalitions
+
+
+def to_binary_class(data, value):
+    """
+    :param data: regular data
+    :param value: the value to be assigned as 1
+    :return: binary classified data
+    """
+    binary_data = data.copy()
+    bool_labels = binary_data[label] == value
+    binary_data[label] = bool_labels
+    return binary_data
+
+
+def get_sorted_vote_division(y):
+    vote_results = dict()
+    for label_name, label_index in label2num.items():
+        percent_of_voters = sum(list(y == label_index)) / len(y)
+        vote_results[label_index] = percent_of_voters
+    return OrderedDict(sorted(vote_results.items(), key=operator.itemgetter(1)))
+
+
+def divide_data(df: DataFrame, data_class=label):
+    x_df = df.loc[:, df.columns != data_class]
+    y_df = df[data_class]
+    return x_df, y_df
+
+
+def categorize_data(df: DataFrame):
+    object_columns = df.keys()[df.dtypes.map(lambda x: x == 'object')]
+    for curr_column in object_columns:
+        df[curr_column] = df[curr_column].astype("category")
+        df[curr_column + '_Int'] = df[curr_column].cat.rename_categories(range(df[curr_column].nunique())).astype('int')
+        df.loc[df[curr_column].isna(), curr_column + '_Int'] = np.nan  # fix NaN conversion
+        df[curr_column] = df[curr_column + '_Int']
+        df = df.drop(curr_column + '_Int', axis=1)
+    return df
+
+
+def split_training_set(df: DataFrame, validation_size: float) -> (
+        DataFrame, DataFrame, DataFrame, DataFrame, DataFrame, DataFrame):
+    val_split = StratifiedShuffleSplit(n_splits=1, test_size=validation_size)
+    x = df.loc[:, df.columns != label]
+    y = df[label]
+    train_index_first, test_index = next(val_split.split(x, y))
+    x_train, x_val, y_train, y_val = x.iloc[train_index_first], x.iloc[test_index], y[train_index_first], y[test_index]
+
+    train = x_train.assign(Vote=y_train.values).reset_index(drop=True)
+    val = x_val.assign(Vote=y_val.values).reset_index(drop=True)
+
+    x = train.loc[:, df.columns != label]
+    y = train[label]
+
+    return train, val
+
+
+def export_to_csv(filespath: str, x_train: DataFrame, x_val: DataFrame,
+                  x_test: DataFrame, y_train: DataFrame, y_val: DataFrame,
+                  y_test: DataFrame, prefix: str):
+    x_train = x_train.assign(Vote=y_train.values)
+    x_val = x_val.assign(Vote=y_val.values)
+    x_test = x_test.assign(Vote=y_test.values)
+    x_train.to_csv(filespath + "{}_train.csv".format(prefix), index=False)
+    x_val.to_csv(filespath + "{}_val.csv".format(prefix), index=False)
+    x_test.to_csv(filespath + "{}_test.csv".format(prefix), index=False)
+
+
+def score(x_train: DataFrame, y_train: DataFrame, clf, k: int):
+    return cross_val_score(clf, x_train, y_train, cv=k, scoring='accuracy').mean()
+
+
+def __distance_num(a, b, r):
+    np.seterr(invalid='ignore')
+    return np.divide(np.abs(np.subtract(a, b)), r)
+
+
+def closest_fit(ref_data, examine_row, local_nominal_features,
+                local_numerical_features):
+    current_nominal_features = [f for f in local_nominal_features if
+                                f in ref_data.columns]
+    data_nominal = ref_data[current_nominal_features]
+    examine_row_obj = examine_row[current_nominal_features].values
+    obj_diff = data_nominal.apply(
+        lambda _row: (_row.values != examine_row_obj).sum(), axis=1)
+
+    num_features = [f for f in local_numerical_features if
+                    f in ref_data.columns]
+    data_numerical = ref_data[num_features]
+    examine_row_numerical = examine_row[num_features]
+    col_max = data_numerical.max().values
+    col_min = data_numerical.min().values
+    r = col_max - col_min
+
+    # replace missing values in examine row to inf in order distance to work
+    examine_row_numerical = examine_row_numerical.replace(np.nan, np.inf)
+
+    num_diff = data_numerical.apply(
+        lambda _row: __distance_num(_row.values, examine_row_numerical.values,
+                                    r), axis=1)
+    for row in num_diff:
+        row[(row == np.inf)] = 1
+
+    num_diff = num_diff.apply(lambda _row: _row.sum())
+
+    total_dist = num_diff + obj_diff
+    return total_dist.reset_index(drop=True).idxmin()
+
+
+def export_to_csv(filepath: str, df: DataFrame):
+    df.to_csv(filepath, index=False)
+
+
+def winner_color(clf, x_test: DataFrame):
+    y_test_proba: np.ndarray = np.average(clf.predict_proba(x_test), axis=0)
+    pred_winner = np.argmax(y_test_proba)
+    print(f"The predicted party to win the elections is {num2label[pred_winner]}")
+    plt.plot(y_test_proba)  # arguments are passed to np.histogram
+    plt.title("Test Vote Probabilities")
+    plt.show()
---
+++ HW_5/data_prepration.py
@@ -0,0 +1,177 @@
+from data_infrastructure import *
+from impyute import imputation
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from collections import defaultdict
+
+
+def negative_2_nan(x_train: DataFrame, x_val: DataFrame,
+                   x_test: DataFrame) -> (DataFrame, DataFrame, DataFrame):
+    for feature in selected_numerical_features:
+        x_train.loc[(~x_train[feature].isnull()) & (
+                x_train[feature] < 0), feature] = np.nan
+        x_val.loc[(~x_val[feature].isnull()) & (
+                x_val[feature] < 0), feature] = np.nan
+        x_test.loc[(~x_test[feature].isnull()) & (
+                x_test[feature] < 0), feature] = np.nan
+    return x_train, x_val, x_test
+
+
+def remove_outliers(x_train: DataFrame, x_val: DataFrame, x_test: DataFrame,
+                    z_threshold: float):
+    mean_train = x_train[selected_normal_features].mean()
+    std_train = x_train[selected_normal_features].std()
+
+    dist_train = (x_train[selected_normal_features] - mean_train) / std_train
+    dist_val = (x_val[selected_normal_features] - mean_train) / std_train
+    dist_test = (x_test[selected_normal_features] - mean_train) / std_train
+
+    data_list = [x_train, x_val, x_test]
+    dist_list = [dist_train, dist_val, dist_test]
+
+    for feature in selected_normal_features:
+        for df, dist in zip(data_list, dist_list):
+            for i in dist[feature].loc[(dist[feature] > z_threshold) | (dist[feature] < -z_threshold)].index:
+                df.at[i, feature] = np.nan
+
+    return x_train, x_val, x_test
+
+
+def get_features_correlation(data: DataFrame,
+                             features_correlation_threshold: float):
+    correlation_dict = defaultdict(list)
+    for f1 in data.columns:
+        for f2 in data.columns:
+            if f2 == f1:
+                continue
+            correlation = data[f1].corr(data[f2], method='pearson')  # calculating pearson correlation
+            if abs(correlation) >= features_correlation_threshold:
+                correlation_dict[f1].append(f2)
+    return correlation_dict
+
+
+def fill_feature_correlation(data: DataFrame, correlation_dict: dict):
+    for f1 in correlation_dict.keys():
+        for f2 in correlation_dict[f1]:
+            coef_val = (data[f2] / data[f1]).mean()
+            # fill values for data set
+            other_approximation = data[f1] * coef_val
+            data[f2].fillna(other_approximation, inplace=True)
+
+
+def closest_fit_imputation(ref_data: DataFrame, data_to_fill: DataFrame):
+    for index, row in data_to_fill[data_to_fill.isnull().any(axis=1)].iterrows():
+        row.fillna(ref_data.iloc[closest_fit(ref_data, row, selected_nominal_features, selected_numerical_features)], inplace=True)
+
+
+def imputations(train: DataFrame, val: DataFrame, test: DataFrame):
+    # fill missing values by using information from correlated features
+    correlation_dict_train = get_features_correlation(train, global_correlation_threshold)
+
+    fill_feature_correlation(train, correlation_dict_train)
+    fill_feature_correlation(val, correlation_dict_train)
+    fill_feature_correlation(test, correlation_dict_train)
+
+    # # fill missing data using closest fit
+    # print("closest fit for train")
+    # closest_fit_imputation(train.dropna(how='any'), train)
+    # print("closest fit for validation")
+    # closest_fit_imputation(val.dropna(how='any'), val)
+    # print("closest fit for test")
+    # closest_fit_imputation(test.dropna(how='any'), test)
+
+    # fill normal distributed features using EM algorithm
+    train_after_em = imputation.cs.em(np.array(train[selected_normal_features]), loops=50, dtype='cont')
+    train.loc[:, selected_normal_features] = train_after_em
+
+    test_after_em = imputation.cs.em(np.array(test[selected_normal_features]), loops=50, dtype='cont')
+    test.loc[:, selected_normal_features] = test_after_em
+
+    val_after_em = imputation.cs.em(np.array(val[selected_normal_features]), loops=50, dtype='cont')
+    val.loc[:, selected_normal_features] = val_after_em
+
+    # fill using statistics
+    # for numerical feature filling by median
+    train[selected_numerical_features] = train[selected_numerical_features].fillna(train[selected_numerical_features].median(),
+                                                                                   inplace=False)
+    val[selected_numerical_features] = val[selected_numerical_features].fillna(val[selected_numerical_features].median(), inplace=False)
+    test[selected_numerical_features] = test[selected_numerical_features].fillna(test[selected_numerical_features].median(), inplace=False)
+
+    # for categorical feature filling by majority
+    train[selected_nominal_features] = train[selected_nominal_features].fillna(
+        train[selected_nominal_features].agg(lambda x: x.value_counts().index[0]),
+        inplace=False)
+    val[selected_nominal_features] = val[selected_nominal_features].fillna(
+        val[selected_nominal_features].agg(lambda x: x.value_counts().index[0]), inplace=False)
+    test[selected_nominal_features] = test[selected_nominal_features].fillna(
+        test[selected_nominal_features].agg(lambda x: x.value_counts().index[0]), inplace=False)
+
+    return train, val, test
+
+
+def normalization(x_train: DataFrame, x_val: DataFrame, x_test: DataFrame):
+    scale_std = StandardScaler()
+    scale_min_max = MinMaxScaler(feature_range=(-1, 1))
+    local_uniform_features = [f for f in selected_uniform_features if f not in selected_nominal_features]
+    x_train[local_uniform_features] = scale_min_max.fit_transform(x_train[local_uniform_features])
+    x_val[local_uniform_features] = scale_min_max.transform(x_val[local_uniform_features])
+    x_test[local_uniform_features] = scale_min_max.transform(x_test[local_uniform_features])
+
+    local_non_uniform = [f for f in selected_features_without_label if
+                         f not in selected_uniform_features and f not in selected_nominal_features]
+
+    x_train[local_non_uniform] = scale_std.fit_transform(x_train[local_non_uniform])
+    x_val[local_non_uniform] = scale_std.transform(x_val[local_non_uniform])
+    x_test[local_non_uniform] = scale_std.transform(x_test[local_non_uniform])
+    return x_train, x_val, x_test
+
+
+def main():
+    df_training_set = load_data(TRAINING_SET_PATH)
+    df_test_set = load_data(TEST_SET_PATH)
+
+    # categorized nominal attributes to int
+    df_training_set = categorize_data(df_training_set)
+    df_test_set = categorize_data(df_test_set)
+
+    # export raw data to csv files
+    train, val = split_training_set(df_training_set, global_validation_size)
+
+    export_to_csv(PATH + "raw_train.csv", train)
+    export_to_csv(PATH + "raw_val.csv", val)
+    export_to_csv(PATH + "raw_test.csv", df_test_set)
+
+    # saving voters ID column, remove it from to set to prevent it effect
+    voters_id_column = df_test_set[voters_id]
+    test = df_test_set.loc[:, df_test_set.columns != voters_id].copy()
+
+    # data cleansing
+    train, val, test = negative_2_nan(train, val, test)
+    train, val, test = remove_outliers(train, val, test, global_z_threshold)
+
+    # imputation
+    train, val, test = imputations(train, val, test)
+
+    # selected features
+    train = train[selected_features]
+    val = val[selected_features]
+    test = test[selected_features_without_label]
+
+
+    # scaling
+    train, val, test = normalization(train, val, test)
+
+    # rewrite voters ID column
+    test[voters_id] = voters_id_column
+
+    train = train.reindex(sorted(train.columns), axis=1)
+    val = val.reindex(sorted(val.columns), axis=1)
+    test = test.reindex(sorted(test.columns), axis=1)
+
+    export_to_csv(TRAIN_PATH, train)
+    export_to_csv(VALIDATION_PATH, val)
+    export_to_csv(TEST_PATH, test)
+
+
+if __name__ == '__main__':
+    main()
